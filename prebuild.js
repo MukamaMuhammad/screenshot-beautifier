ec94a62f5fdaca9bca9a96e604a52103a3d1b5c7
lolenti <lolentimuhammad2@gmail.com>
2024-01-08 06:02:20 +0300

    500 Internal server error fix

--- prebuild.js
+++ prebuild.js
@@ -3,21 +3,19 @@ const path = require("node:path");
 const fs = require("fs");
 const baseDir = process.cwd();
 
-// /node_modules/@netlify/plugin-nextjs/lib/templates/requireHooks.js
-
 const prebuildScripts = async () => {
   const file = path.join(
     baseDir,
     "/node_modules",
-    "@netlify/plugin-nextjs/lib/templates/requireHooks.js"
+    "next/dist/server/require-hook.js"
   );
 
   const content = await fs.promises.readFile(file, "utf-8");
   await fs.promises.writeFile(
     file,
     content.replace(
-      "const reactMode = process.env.__NEXT_PRIVATE_PREBUNDLED_REACT || 'default';",
-      "const reactMode = process.env.__NEXT_PRIVATE_PREBUNDLED_REACT || 'next';"
+      "if (process.env.__NEXT_PRIVATE_PREBUNDLED_REACT) {",
+      "if (true) {"
     )
   );
 };
