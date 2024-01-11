b2977b44162d9b4b9dea2feab88962a2c170392a
lolenti <lolentimuhammad2@gmail.com>
2024-01-12 00:31:09 +0300

    Lemon squeezy test mode removed. blog added. and some other small additions

--- app/blog/create/page.jsx
+++ app/blog/create/page.jsx
@@ -5,8 +5,8 @@ import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
-import ReactQuill from "react-quill";
-import "react-quill/dist/quill.snow.css";
+import Editor from "@components/Editor";
+
 import ProtectedRouter from "@components/ProtectedRouters/ProtectedRoute";
 
 const page = () => {
@@ -15,32 +15,6 @@ const page = () => {
   const [title, setTitle] = useState("");
   const [image, setImage] = useState("");
   const [content, setContent] = useState("");
-
-  const modules = {
-    toolbar: [
-      [{ header: [1, 2, false] }],
-      ["bold", "italic", "underline", "strike", "blockquote"],
-      [{ list: "ordered" }, { list: "bullet" }],
-      ["link", "color", "image"],
-      [{ "code-block": true }],
-      ["clean"],
-    ],
-  };
-  const formats = [
-    "header",
-    "bold",
-    "italic",
-    "underline",
-    "strike",
-    "blockquote",
-    "list",
-    "bullet",
-    "link",
-    "indent",
-    "image",
-    "code-block",
-    "color",
-  ];
 
   const handleFormSubmit = async (e) => {
     e.preventDefault();
@@ -62,51 +36,43 @@ const page = () => {
   };
 
   return (
-    // <ProtectedRouter>
-    <section className="container md:px-5 py-10 mx-auto  ">
-      <div className="lg:px-40">
-        <form onSubmit={handleFormSubmit} className="flex flex-col gap-5 ">
-          <div className="grid w-full  items-center gap-1.5">
-            <Label htmlFor="email">Title</Label>
-            <Input
-              type="text"
-              id="title"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-              placeholder="Title"
-              className="rounded-none text-black"
-            />
-          </div>
-          <div className="grid w-full  items-center gap-1.5">
-            <Label htmlFor="Image">Image URL</Label>
-            <Input
-              type="text"
-              id="Image"
-              value={image}
-              onChange={(e) => setImage(e.target.value)}
-              placeholder="https://example.com"
-              className="rounded-none text-black"
-            />
-          </div>
-          <div>
-            <Label>Content</Label>
-            <ReactQuill
-              theme="snow"
-              value={content}
-              onChange={setContent}
-              modules={modules}
-              formats={formats}
-              className="bg-white text-black"
-              placeholder="Write somethings..."
-            />
-          </div>
-          <div>
-            <Button type="submit">Publish</Button>
-          </div>
-        </form>
-      </div>
-    </section>
-    // </ProtectedRouter>
+    <ProtectedRouter>
+      <section className="container md:px-5 py-10 mx-auto  ">
+        <div className="lg:px-40">
+          <form onSubmit={handleFormSubmit} className="flex flex-col gap-5 ">
+            <div className="grid w-full  items-center gap-1.5">
+              <Label htmlFor="email">Title</Label>
+              <Input
+                type="text"
+                id="title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                placeholder="Title"
+                className="rounded-none text-black"
+              />
+            </div>
+            <div className="grid w-full  items-center gap-1.5">
+              <Label htmlFor="Image">Image URL</Label>
+              <Input
+                type="text"
+                id="Image"
+                value={image}
+                onChange={(e) => setImage(e.target.value)}
+                placeholder="https://example.com"
+                className="rounded-none text-black"
+              />
+            </div>
+            <div>
+              <Label>Content</Label>
+              <Editor setContent={setContent} />
+            </div>
+            <div>
+              <Button type="submit">Publish</Button>
+            </div>
+          </form>
+        </div>
+      </section>
+    </ProtectedRouter>
   );
 };
 
