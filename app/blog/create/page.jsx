23e25eec211bf8dc3d2bd42ceb3ddec241f56cd2
lolenti <lolentimuhammad2@gmail.com>
2024-01-12 00:12:01 +0300

    Lemon squeezy test mode removed. blog added. and some other small additions

--- app/blog/create/page.jsx
+++ app/blog/create/page.jsx
@@ -62,51 +62,51 @@ const page = () => {
   };
 
   return (
-    <ProtectedRouter>
-      <section className="container md:px-5 py-10 mx-auto  ">
-        <div className="lg:px-40">
-          <form onSubmit={handleFormSubmit} className="flex flex-col gap-5 ">
-            <div className="grid w-full  items-center gap-1.5">
-              <Label htmlFor="email">Title</Label>
-              <Input
-                type="text"
-                id="title"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-                placeholder="Title"
-                className="rounded-none text-black"
-              />
-            </div>
-            <div className="grid w-full  items-center gap-1.5">
-              <Label htmlFor="Image">Image URL</Label>
-              <Input
-                type="text"
-                id="Image"
-                value={image}
-                onChange={(e) => setImage(e.target.value)}
-                placeholder="https://example.com"
-                className="rounded-none text-black"
-              />
-            </div>
-            <div>
-              <Label>Content</Label>
-              <ReactQuill
-                theme="snow"
-                value={content}
-                onChange={setContent}
-                modules={modules}
-                formats={formats}
-                className="bg-white text-black"
-                placeholder="Write somethings..."
-              />
-            </div>
-            <div>
-              <Button type="submit">Publish</Button>
-            </div>
-          </form>
-        </div>
-      </section>
-    </ProtectedRouter>
+    // <ProtectedRouter>
+    <section className="container md:px-5 py-10 mx-auto  ">
+      <div className="lg:px-40">
+        <form onSubmit={handleFormSubmit} className="flex flex-col gap-5 ">
+          <div className="grid w-full  items-center gap-1.5">
+            <Label htmlFor="email">Title</Label>
+            <Input
+              type="text"
+              id="title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              placeholder="Title"
+              className="rounded-none text-black"
+            />
+          </div>
+          <div className="grid w-full  items-center gap-1.5">
+            <Label htmlFor="Image">Image URL</Label>
+            <Input
+              type="text"
+              id="Image"
+              value={image}
+              onChange={(e) => setImage(e.target.value)}
+              placeholder="https://example.com"
+              className="rounded-none text-black"
+            />
+          </div>
+          <div>
+            <Label>Content</Label>
+            <ReactQuill
+              theme="snow"
+              value={content}
+              onChange={setContent}
+              modules={modules}
+              formats={formats}
+              className="bg-white text-black"
+              placeholder="Write somethings..."
+            />
+          </div>
+          <div>
+            <Button type="submit">Publish</Button>
+          </div>
+        </form>
+      </div>
+    </section>
+    // </ProtectedRouter>
   );
 };
 
